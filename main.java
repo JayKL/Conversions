f23c88ee658a66dd48271463bf1a24fc5c09e030
JayKL <Jaykonrad-lee@hotmail.com>
2019-04-15 15:37:51 +0100

    added conversion program

---
+++ main.java
@@ -0,0 +1,7 @@
+class main{
+		public static void main(String xyz[]){
+		Conversions x=new Conversions();
+		int a=8;
+		System.out.print("and that binary in decimal is: " + x.binaryToDecimal(x.decimalToBinary(a)));
+		}
+}
